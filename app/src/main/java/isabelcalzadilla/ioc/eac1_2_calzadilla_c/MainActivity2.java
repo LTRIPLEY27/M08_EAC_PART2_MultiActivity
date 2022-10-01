455590c9391916e15d7099dc5f7ea5b2db52834f
isaDevs <isabel.calzadilla.18@gmail.com>
2022-10-01 18:51:39 +0200

    COMMIT ACTIVITY WITH INTENT

---
+++ app/src/main/java/isabelcalzadilla/ioc/eac1_2_calzadilla_c/MainActivity2.java
@@ -0,0 +1,13 @@
+package isabelcalzadilla.ioc.eac1_2_calzadilla_c;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
