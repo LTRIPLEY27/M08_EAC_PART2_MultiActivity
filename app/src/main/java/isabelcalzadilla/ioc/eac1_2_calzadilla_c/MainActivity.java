4ecd391c8c3288d0bf308eb491eda44886891c81
isaDevs <isabel.calzadilla.18@gmail.com>
2022-10-01 17:36:46 +0200

    EAC1, Part 2, first activity

---
+++ app/src/main/java/isabelcalzadilla/ioc/eac1_2_calzadilla_c/MainActivity.java
@@ -0,0 +1,13 @@
+package isabelcalzadilla.ioc.eac1_2_calzadilla_c;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
