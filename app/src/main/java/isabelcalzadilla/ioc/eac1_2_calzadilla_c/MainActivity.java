481281be785bef65832dc4e0775623771e8bc607
isaDevs <isabel.calzadilla.18@gmail.com>
2022-10-01 18:50:57 +0200

    COMMIT ACTIVITY WITH INTENT

--- app/src/main/java/isabelcalzadilla/ioc/eac1_2_calzadilla_c/MainActivity.java
+++ app/src/main/java/isabelcalzadilla/ioc/eac1_2_calzadilla_c/MainActivity.java
@@ -1,13 +1,33 @@
 package isabelcalzadilla.ioc.eac1_2_calzadilla_c;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button enterButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // INICIALIZACIÓN DEL BUTTON
+        enterButton = findViewById(R.id.enter_button);
+
+        // LLAMADO AL MÉTODO CON EL BUTTON PARA FORZAR EL INTENT
+        enterButton.setOnClickListener(this::callToActivityTwo);
+    }
+
+
+    // MÉTODO QUE TOMARÁ LA ACCIÓN DEL BUTTON Y LLAMARÁ A LA SEGUNDA ACTIVITY
+    public void callToActivityTwo(View activityTwo){
+
+        Intent actTwo = new Intent(this, MainActivity2.class);
+
+        startActivity(actTwo);
     }
 }
